f730397fe5912303a22db5da9fae25f0a3fb49dc
htppp <39856549+htppp@users.noreply.github.com>
2021-10-18 00:31:56 +0900

    clasp pushed on 2021.10.18.00.31.56

--- コード.js
+++ コード.js
@@ -20,7 +20,7 @@ const cellColumnNum = {
 
 // 技名からその技のデータが書いてある行の番号を取得する シート数値計算を参照する
 function GetRowNumberOfAction( actionName ) {
-	for( var i = 20; i < actions.length; i++ ) { // 行21からアクション名が入っている
+	for( var i = 21; i < actions.length; i++ ) { // 行21からアクション名が入っている
 		if( actions[ i ][ 1 ] === actionName ) { // 列Bにアクション名が入っている
 			return i;
 		}
