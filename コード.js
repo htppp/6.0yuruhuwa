c51a77121a436447ad96c07413d8b97a77d46bfa
htppp <39856549+htppp@users.noreply.github.com>
2021-10-17 23:47:00 +0900

    clasp pushed on 2021.10.17.23.47.00

--- コード.js
+++ コード.js
@@ -86,9 +86,9 @@ function RecalcCell( editRow, editColumn ) {
 	    "RecalcCell : called. " +
 	    "( editRow, editColumn ) = " +
 	    "( " + editRow + ", " + editColumn + " )" );
-	var r = sheet_tl.getRange( editRow, editColumn );
-	Logger.log( "r = " + r );
-	var value = r.getValue(); // セルの値を取得
+	var values = sheet_tl.getRange( editRow, editColumn ).getValues(); // セルの値を取得
+	var value  = sheet_tl.getRange( editRow, editColumn ).getValue();  // セルの値を取得
+	Logger.log( "values = " + values );
 	Logger.log( "value = " + value );
 	Logger.log( "value.slice( 0, 1 ) = " + value.slice( 0, 1 ) );
 	Logger.log( "value.slice( 0, 1 ) != \"=\" " + value.slice( 0, 1 ) != "=" );
