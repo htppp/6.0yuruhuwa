eb984ecd64d19c46d489c8141bd12d74fc56c6cb
htppp <39856549+htppp@users.noreply.github.com>
2021-10-17 23:03:40 +0900

    clasp pushed on 2021.10.17.23.03.40

--- コード.js
+++ コード.js
@@ -97,6 +97,9 @@ function CalcCell( editRow, editColumn ) {
 	Logger.log( "CalcCell : editRow >= 2" + editRow >= 2 );
 	Logger.log( "CalcCell : editColumn >= 8" + editColumn >= 8 );
 	Logger.log( "CalcCell : editColumn <= 36" + editColumn <= 36 );
+	Logger.log( "CalcCell : Number(editRow) >= 2" + Number( editRow ) >= 2 );
+	Logger.log( "CalcCell : Number(editColumn) >= 8" + Number( editColumn ) >= 8 );
+	Logger.log( "CalcCell : Number(editColumn) <= 36" + Number( editColumn ) <= 36 );
 
 	if( editRow >= 2 && ( editColumn >= 8 && editColumn <= 36 ) ) {
 		// T1の軽減 H,I,J (8,9,10)
